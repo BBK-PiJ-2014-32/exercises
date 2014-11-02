a1fe53fe1f49caf8c732bf2a4641b96baa946907
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-02 17:04:13 +0000

    first commit

---
+++ Calculator.java
@@ -0,0 +1,49 @@
+public class Calculator{
+
+	private int x;
+	private int y;
+
+			public void add(int x, int y){
+				this.x = x;
+				this.y = y;
+
+				int result = x + y;
+				System.out.println(result);
+				}
+
+		public void subtract(int x, int y){
+				this.x = x;
+				this.y = y;
+
+				int result = x - y;
+				System.out.println(result);
+				}
+
+		public void multiply(int x, int y){
+				this.x = x;
+				this.y = y;
+
+				int result = x * y;
+				System.out.println(result);
+				}
+
+		public void divide(int x, int y){
+				this.x = x;
+				this.y = y;
+
+				double a = (double) x;
+				double b = (double) y;
+
+				double result = a / b;
+				System.out.println(result);
+				}
+
+		public void modulus(int x, int y){
+				this.x = x;
+				this.y = y;
+
+				int result = x % y;
+				System.out.println(result);
+				}
+
+	}
