06ef07e2dc176adef8a741279fdbd5c75d0e26c7
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-03 13:56:33 +0000

    Array length and array print out refined

--- ArrayCopier.java
+++ ArrayCopier.java
@@ -12,10 +12,24 @@ public class ArrayCopier {
 
 		public void copy(int[] src, int[] dst){
 					if (src.length == dst.length) {
-						for (int i = 1; i <=  src.length; i++)	{
+						for (int i = 0; i < src.length; i++)	{
 							dst[i] = src[i];
 						}
-				} System.out.println(Arrays.toString(dst));
+					System.out.println("The arrays are equal therefore the new array is: " + Arrays.toString(dst));
+
+				} else if (src.length > dst.length) {
+						for (int i = 0; i <  dst.length; i++)	{
+							dst[i] = src[i];
+						}
+					System.out.println("Array 1 is longer than array 2 therefore the new array is: " + Arrays.toString(dst));
+
+				}else if (src.length < dst.length) {
+						for (int i = 0; i <  src.length; i++)	{
+							dst[i] = src[i];
+						}
+					System.out.println("Array 1 is shorter than array 2 therefore the new array is: " + Arrays.toString(dst));
+			}
+
 
 		}
 
