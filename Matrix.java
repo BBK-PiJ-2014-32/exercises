45d01c7549c8d74424007f4179a6fa17da0a2a51
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-03 23:16:34 +0000

    setRow method added

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Matrix {
 
@@ -16,15 +17,38 @@ public class Matrix {
 								}
 				}
 
-			public void setElement(int x, int y, int z){
+			public int[][] setElement(int x, int y, int z){
 
 						if (x > myMatrix.length || y > myMatrix[x].length){
 							System.out.println("Invalid");
+							return myMatrix;
 
 						}
 						myMatrix[x][y] = z;
-
+						return myMatrix;
 					}
+
+			public int[][] setRow(int x, String str){
+					if (x < myMatrix.length || str.length() < myMatrix[x].length){
+
+							Scanner scanner = new Scanner(str);
+
+								while (scanner.hasNext()){
+
+										if (scanner.hasNextInt()){
+											myMatrix[x][myMatrix[x].length-1] = scanner.nextInt();
+										}
+								scanner.close();
+								return myMatrix;
+					}
+					return myMatrix;
+				}
+
+
+					System.out.println("Invalid");
+					return myMatrix;
+			}
+
 
 
 	}
