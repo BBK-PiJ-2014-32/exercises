296dbfdc5ee2c96cf340c910c20c75aaa2736bb4
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-06 11:28:04 +0000

    first commit

---
+++ MatrixChecker.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MatrixChecker {
+
+	private int[][] myMatrix;
+
+
+		public boolean isSymmetrical(int[]) {
+
+		}
+
+		public boolean isSymmetrical(int[][]) {
+
+		}
+
+		public isTriagular(int[][]){
+
+		}
