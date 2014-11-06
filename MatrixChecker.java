8502d6b3ec6916145e51d109941a3db085c0abad
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-06 12:16:53 +0000

    first isSymmetrical method added

--- MatrixChecker.java
+++ MatrixChecker.java
@@ -3,17 +3,34 @@ import java.util.Scanner;
 
 public class MatrixChecker {
 
+	private int[] myArray;
 	private int[][] myMatrix;
 
+		public MatrixChecker(){
 
-		public boolean isSymmetrical(int[]) {
-
+				this.myArray = myArray;
+				this.myMatrix = myMatrix;
 		}
 
-		public boolean isSymmetrical(int[][]) {
+			public boolean isSymmetrical(int[] myArray) {
+						boolean result = false;
 
-		}
+						for(int i=0; i < myMatrix.length; i++){
+							if (myMatrix[i] == myMatrix[myMatrix.length-1]){
+									result = true;
+							}else {
+									result = false;
+							}
+					}
+					return result;
+			}
 
-		public isTriagular(int[][]){
+			public boolean isSymmetrical(int[][] myMatrix) {
+						return true;
+			}
 
-		}
+			public boolean isTriagular(int[][] myMatrix){
+						return true;
+			}
+
+}
