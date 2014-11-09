868f86a4b89cedc09c004de5fe3ad25189012333
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-09 20:27:52 +0000

    all methods added

--- Spy.java
+++ Spy.java
@@ -1,16 +1,34 @@
-public class spy{
-	private static in spyCount = 0;
+public class Spy{
 
-	public Spy(){
-		spyCount++;
+	private static int spyCount = 0;
+	private int spyID;
 
+	public Spy(int x){
+				this.spyID = spyID;
+				spyID = x;
+				spyCount++;
+				System.out.println("New spy: " + spyID + ", registered.");
+				System.out.println("There are currently: " + spyCount + " spies.");
 		}
 
-		public static in getNumberOfSpies(){
-			return spyCount;
+		public static int getNumberOfSpies(){
+				return spyCount;
 			}
 
 		public void die(){
-			spyCount--;
+				this.spyID = spyID;
+				spyCount--;
+				System.out.println("Spy " + spyID + " has been detected and eliminated");
+			}
+
+	public static void main(String [] args){
+
+				Spy spy1 = new Spy(12);
+				Spy spy2 = new Spy(123);
+				Spy spy3 = new Spy(007);
+				Spy spy4 = new Spy(6);
+
+				spy4.die();
+				spy1.die();
 			}
 	}
