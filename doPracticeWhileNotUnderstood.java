a7dab8c4017dfe87fdd0b68530bdbe7c4a45e7f6
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-09 23:14:04 +0000

    method added

--- doPracticeWhileNotUnderstood.java
+++ doPracticeWhileNotUnderstood.java
@@ -1,19 +1,48 @@
-public class Mark {
+public class doPracticeWhileNotUnderstood {
 
-	private int mark;
-	private int stuNumber;
-	private int dists;
-	private int passes;
-	private int fails;
+	//private int mark;
+	//private int stuNumber;
+	//private int dists;
+	//private int passes;
+	//private int fails;
 
-	public Mark(){
+	public doPracticeWhileNotUnderstood(){
+
+			//this.mark = mark;
+			//this.stuNumber = stuNumber;
+			//this.dists = dists;
+			//this.passes = passes;
+			//this.fails = fails;
 
 	}
 
+	public void markInputOutput(){
 
+			int markIn;
+			int stu = 0;
+			int dist = 0;
+			int pass = 0;
+			int fail = 0;
+			int inval = 0;
 
+				do {
+					System.out.print("Input a mark: ");
+					String str = System.console().readLine();
+					markIn = Integer.parseInt(str);
+					stu++;
+						if (markIn >= 70 && markIn <= 100){
+								dist++;
+					} else if (markIn >= 50 && markIn <= 69){
+								pass++;
+					} else if (markIn >= 0 && markIn <= 49){
+								fail++;
+					} else	{
+								inval++;
+					}
+				}
+				while(markIn != -1);
 
+	System.out.println("There are " + stu + " students: " + dist + " distinctions, " + 1 + " pass, " + fail + " fails (plus " + inval + " invalid).");
 
-
-
+		}
 }
