b3c365867763eafad4651d8f06c037f22a8cb3aa
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-10 13:45:39 +0000

    first commit2

---
+++ HospitalManager.java
@@ -0,0 +1,30 @@
+public class HospitalManager{
+
+	private Patient patientListStart = null;
+
+	public static void main(String [] args){
+		//add patients.
+			Patient pat1 = new Patient("John", 45, "Knee pain");
+			patientListStart = pat1;
+			Patient pat2 = new Patient("Laura", 22, "Head pain");
+			patientListStart = pat2;
+			Patient pat3 = new Patient("Mike", 30, "Sore throat");
+			patientListStart = pat3;
+			Patient pat4 = new Patient("Dave", 41, "Chest infection");
+			patientListStart = pat4;
+			Patient pat5 = new Patient("Sarah", 18, "Leg pain");
+			patientListStart = pat5;
+			Patient pat6 = new Patient("George", 12, "Slipped disc");
+			patientListStart = pat6;
+			Patient pat7 = new Patient("Steve", 75, "Rash");
+			patientListStart = pat7;
+			Patient pat8 = new Patient("Maria", 36, "Acne");
+			patientListStart = pat8;
+			Patient pat9 = new Patient("Jane", 25, "Broken leg");
+			patientListStart = pat9;
+			Patient pat10 = new Patient("Racheal", 26, "Stomach pain");
+			patientListStart = pat10;
+	}
+
+
+}
