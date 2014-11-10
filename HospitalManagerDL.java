99c7442c8a1687c45667c4f4b8122bf2a45e2d4a
unknown <phanna02@CSPC066.dcs.bbk.ac.uk>
2014-11-10 19:47:46 +0000

    traverse print out fixed

--- HospitalManagerDL.java
+++ HospitalManagerDL.java
@@ -1,51 +1,58 @@
-public class HospitalManager{
-	private Patient patientListStart = null;
+public class HospitalManagerDL{
+	private PatientDL patientListStart = null;
+	private PatientDL patientListEnd = null;
 
 	public static void main(String [] args){
 
-		HospitalManager hm = new HospitalManager();
+		HospitalManagerDL hm = new HospitalManagerDL();
 		hm.launch();
 	}
 
 		//add patients.
 		public void launch(){
-			Patient pat1 = new Patient("John", 45, "Knee pain");
+			PatientDL pat1 = new PatientDL("John", 45, "Knee pain");
 			patientListStart = pat1;
-			Patient pat2 = new Patient("Laura", 22, "Head pain");
+			PatientDL pat2 = new PatientDL("Laura", 22, "Head pain");
 			patientListStart.addPatient(pat2);
-			Patient pat3 = new Patient("Mike", 30, "Sore throat");
+			PatientDL pat3 = new PatientDL("Mike", 30, "Sore throat");
 			patientListStart.addPatient(pat3);
-			Patient pat4 = new Patient("Dave", 41, "Chest infection");
+			PatientDL pat4 = new PatientDL("Dave", 41, "Chest infection");
 			patientListStart.addPatient(pat4);
-			Patient pat5 = new Patient("Sarah", 18, "Leg pain");
+			PatientDL pat5 = new PatientDL("Sarah", 18, "Leg pain");
 			patientListStart.addPatient(pat5);
-			Patient pat6 = new Patient("George", 12, "Slipped disc");
+			PatientDL pat6 = new PatientDL("George", 12, "Slipped disc");
 			patientListStart.addPatient(pat6);
-			Patient pat7 = new Patient("Steve", 75, "Rash");
+			PatientDL pat7 = new PatientDL("Steve", 75, "Rash");
 			patientListStart.addPatient(pat7);
-			Patient pat8 = new Patient("Maria", 36, "Acne");
+			PatientDL pat8 = new PatientDL("Maria", 36, "Acne");
 			patientListStart.addPatient(pat8);
-			Patient pat9 = new Patient("Jane", 25, "Broken leg");
+			PatientDL pat9 = new PatientDL("Jane", 25, "Broken leg");
 			patientListStart.addPatient(pat9);
-			Patient pat10 = new Patient("Rachael", 26, "Stomach pain");
+			PatientDL pat10 = new PatientDL("Rachael", 26, "Stomach pain");
 			patientListStart.addPatient(pat10);
+			patientListEnd = pat10;
 
 			//print out list
 			patientListStart.printList();
-			//change list start pointer.
-			patientListStart = pat2;
-			patientListStart.deletePatient(pat1);
+			patientListEnd.printListRev();
+
+
 			patientListStart.deletePatient(pat7);
-			System.out.println();
+			//System.out.println();
 			patientListStart.printList();
 
 			//count patients in list recursively.
-			int count = patientListStart.queueLengthRec();
-			System.out.println("There are " + count + " patients in the queue.");
+			//int count = patientListStart.queueLengthRec();
+			//System.out.println("There are " + count + " patients in the queue.");
 
 			//count patients in the list iteratively.
-			int count2 = patientListStart.queueLengthIter();
-			System.out.println("There are " + count + " patients in the queue.");
+			//int count2 = patientListStart.queueLengthIter();
+			//System.out.println("There are " + count + " patients in the queue.");
+
+			//pat2.printElement();
+			//pat7.printElement();
+			//pat9.printElement();
+
 
 	}
 
