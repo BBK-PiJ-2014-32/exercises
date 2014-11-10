14bb9fb5f50b8333c59a8346fa191c8d5e101000
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-10 14:50:44 +0000

    print method added

--- Patient.java
+++ Patient.java
@@ -1,7 +1,8 @@
 public class Patient{
 		private String name;
 		private int age;
-		private Sting illness;
+		private String illness;
+		private Patient nextPatient;
 
 		public Patient(String name, int age, String illness){
 				this.name = name;
@@ -10,12 +11,30 @@ public class Patient{
 				this.nextPatient = null;
 		}
 		//add patient method.
-		public void addPatient(Patient new Patient){
+		public void addPatient(Patient newPatient){
 				if (this.nextPatient == null){
 					this.nextPatient = newPatient;
 			} else {
 				this.nextPatient.addPatient(newPatient);
+			}
 		}
 
+		//delete patient method.
+		public boolean deletePatient(Patient patient){
+				if (this.nextPatient == null){
+					return false;
+				} else if (this.nextPatient.name.equals(patient.name)){
+					this.nextPatient = nextPatient.nextPatient;
+					return true;
+				} else {
+					return this.nextPatient.deletePatient(patient);
+				}
 		}
-	}
+
+		public void printList(){
+				System.out.println(this.name + this.age + this.illness);
+					if (nextPatient != null){
+						nextPatient.printList();
+						}
+		}
+}
