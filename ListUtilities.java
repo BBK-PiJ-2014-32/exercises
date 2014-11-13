bbd8d61c10a2477581838da6e416356ad79031bf
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-13 14:57:32 +0000

    first commit

---
+++ ListUtilities.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class ListUtilities {
+
+	private String [] arrayIn;
+	private String name;
+	private ListUtilities head;
+
+
+		public ListUtilities (String [] arrayIn, String name){
+				this.arrayIn = arrayIn;
+				this.name = name;
+
+		}
+}
