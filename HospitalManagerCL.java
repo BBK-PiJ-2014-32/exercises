b395fa93b3b50596563839177f203145d8278782
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-13 12:54:39 +0000

    Static variable added

--- HospitalManagerCL.java
+++ HospitalManagerCL.java
@@ -1,53 +1,59 @@
-public class HospitalManager{
-	private Patient patientListStart = null;
+public class HospitalManagerCL{
+	private static PatientCL patientListStart = null;
 
 	public static void main(String [] args){
 
-		HospitalManager hm = new HospitalManager();
+		HospitalManagerCL hm = new HospitalManagerCL();
 		hm.launch();
 	}
 
 		//add patients.
 		public void launch(){
-			Patient pat1 = new Patient("John", 45, "Knee pain");
+			PatientCL pat1 = new PatientCL("John", 45, "Knee pain");
 			patientListStart = pat1;
-			Patient pat2 = new Patient("Laura", 22, "Head pain");
+			//patientListStart.addPatient(pat1);
+			PatientCL pat2 = new PatientCL("Laura", 22, "Head pain");
 			patientListStart.addPatient(pat2);
-			Patient pat3 = new Patient("Mike", 30, "Sore throat");
+			PatientCL pat3 = new PatientCL("Mike", 30, "Sore throat");
 			patientListStart.addPatient(pat3);
-			Patient pat4 = new Patient("Dave", 41, "Chest infection");
-			patientListStart.addPatient(pat4);
-			Patient pat5 = new Patient("Sarah", 18, "Leg pain");
-			patientListStart.addPatient(pat5);
-			Patient pat6 = new Patient("George", 12, "Slipped disc");
-			patientListStart.addPatient(pat6);
-			Patient pat7 = new Patient("Steve", 75, "Rash");
-			patientListStart.addPatient(pat7);
-			Patient pat8 = new Patient("Maria", 36, "Acne");
-			patientListStart.addPatient(pat8);
-			Patient pat9 = new Patient("Jane", 25, "Broken leg");
-			patientListStart.addPatient(pat9);
-			Patient pat10 = new Patient("Rachael", 26, "Stomach pain");
-			patientListStart.addPatient(pat10);
+			//PatientCL pat4 = new PatientCL("Dave", 41, "Chest infection");
+			//patientListStart.addPatient(pat4);
+			//PatientCL pat5 = new PatientCL("Sarah", 18, "Leg pain");
+			//patientListStart.addPatient(pat5);
+			//PatientCL pat6 = new PatientCL("George", 12, "Slipped disc");
+			pat2.printElement();
+			pat3.printElement();
+			//patientListStart.addPatient(pat6);
+			//PatientCL pat7 = new PatientCL("Steve", 75, "Rash");
+			//patientListStart.addPatient(pat7);
+			//PatientCL pat8 = new PatientCL("Maria", 36, "Acne");
+			//patientListStart.addPatient(pat8);
+			//PatientCL pat9 = new PatientCL("Jane", 25, "Broken leg");
+			//patientListStart.addPatient(pat9);
+			//PatientCL pat10 = new PatientCL("Rachael", 26, "Stomach pain");
+			//patientListStart.addPatient(pat10);
 
 			//print out list
-			patientListStart.printList();
+			//patientListStart.printList();
 			//change list start pointer.
-			patientListStart = pat2;
-			patientListStart.deletePatient(pat1);
-			patientListStart.deletePatient(pat7);
-			System.out.println();
-			patientListStart.printList();
+			//patientListStart = pat2;
+			//patientListStart.deletePatient(pat1);
+			//patientListStart.deletePatient(pat7);
+			//System.out.println();
+			//patientListStart.printList();
 
 			//count patients in list recursively.
-			int count = patientListStart.queueLengthRec();
-			System.out.println("There are " + count + " patients in the queue.");
+			//int count = patientListStart.queueLengthRec();
+			//System.out.println("There are " + count + " patients in the queue.");
 
 			//count patients in the list iteratively.
-			int count2 = patientListStart.queueLengthIter();
-			System.out.println("There are " + count + " patients in the queue.");
+			//int count2 = patientListStart.queueLengthIter();
+			//System.out.println("There are " + count + " patients in the queue.");
+
 
 	}
 
-
+		public static PatientCL getPatientListStart() {
+			return patientListStart;
+	}
 }
