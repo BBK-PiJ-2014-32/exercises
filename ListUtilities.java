63a343af5b3bad937a0dcd04fef6cc59c3556551
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-13 16:01:49 +0000

    array changed to int

--- ListUtilities.java
+++ ListUtilities.java
@@ -11,6 +11,21 @@ public class ListUtilities {
 		public ListUtilities (String [] arrayIn, String name){
 				this.arrayIn = arrayIn;
 				this.name = name;
-
+				this.head = null;
 		}
+
+		public static LinkedList<String> newList(String [] arrayIn){
+					LinkedList<String> finalList = new LinkedList<String>();
+					for(int i = 0; i < arrayIn.length; i++){
+						finalList.add(i, new String(arrayIn[i]));
+					}
+
+
+
+					return finalList;
+			}
+
+
+
+
 }
