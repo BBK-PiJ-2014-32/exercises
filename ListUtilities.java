3067932274f2e3bd6b3bb32166ba453649b9b53e
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-14 11:51:57 +0000

    util LinkedList removed

--- ListUtilities.java
+++ ListUtilities.java
@@ -1,31 +1,18 @@
-import java.util.LinkedList;
 import java.util.ArrayList;
 
 public class ListUtilities {
 
-	private int [] arrayIn;
-	//private int ID;
-	private ListUtilities head;
-
-
-		public ListUtilities (int [] arrayInD){
-				this.arrayIn = arrayIn;
-				//this.ID = ID;
-				this.head = null;
-		}
-
-		public static LinkedList<Integer> newList(int [] arrayIn){
-					LinkedList<Integer> finalList = new LinkedList<Integer>();
+		public static IntegerNode newList(int [] arrayIn){
+					IntegerNode finalList = new IntegerNode(arrayIn[0]);
 					for(int i = 0; i < arrayIn.length; i++){
-						finalList.add(i, new Integer(arrayIn[i]));
-
+						IntegerNode addItem = new IntegerNode(arrayIn[i]);
+						finalList.setNext(addItem);
 					}
 
 
-
+					finalList.printList();
 					return finalList;
 			}
-
 
 
 
