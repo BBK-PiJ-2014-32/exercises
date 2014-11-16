eeb081b577f8d6e6b7705b5c2844dfdad43154d7
Phil Hannant <philhannant@phil-hannants-macbook.local>
2014-11-16 22:57:47 +0000

    first commit

---
+++ Supermarket.java
@@ -0,0 +1,19 @@
+public class Supermarket {
+		
+		public static void main(String [] args){
+			Person per1 = new Person("John", "Doe");
+			Person per1 = new Person("Simon", "Says");
+			
+			
+				
+				
+		}
+		
+		public void addPerson(Person person){
+			
+		}
+
+		public void servePerson(){
+			
+		}
+}
