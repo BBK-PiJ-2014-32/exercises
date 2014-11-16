321e6701fd69aba37dba805c455867c9b922ebb0
Phil Hannant <philhannant@phil-hannants-macbook.local>
2014-11-16 22:58:11 +0000

    first commit

---
+++ Person.java
@@ -0,0 +1,16 @@
+public class Person implements PersonQueue { 
+	private String name;
+
+	
+	public Person(String name){
+		this.name = name;
+	}
+	
+	public void insert(Person person){
+		
+	}
+	public Person retrieve(){
+		
+	}
+
+}
