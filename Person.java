0dbd7df2be93498f7c360e4d88a09b7a2d704ef8
unknown <phanna02@CSPC063.dcs.bbk.ac.uk>
2014-11-17 21:02:02 +0000

    end of lab

--- Person.java
+++ Person.java
@@ -1,17 +1,41 @@
-public class Person implements PersonQueue { 
+public class Person implements PersonQueue {
 	private String name;
+	private Person head = null;
+	private Person next = null;
 
-	
 	public Person(String name){
+		this.head = head;
 		this.name = name;
-	}
-	
+		this.next = next;
+		}
+
 	public void insert(Person person){
-		PersonNode per = new PersonNode(person);
-		PersonNode.setNext(per);
-	}
+			if (this.head == null){
+					this.head = person;
+			} else {
+					if (this.head.getNext() == null){
+							this.head.setNext(person);
+					} else {
+				  			this.head.setNext(person);
+					}
+			}
+		}
+
 	public Person retrieve(){
-		
+		return this;
 	}
+
+	public void setNext(Person per) {
+		  		if (this.next == null){
+					this.next = per;
+			} else {
+		  			this.next.setNext(per);
+			}
+    }
+
+	public Person getNext() {
+		  return next;
+	    }
+
 
 }
