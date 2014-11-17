6ced1d74c0f58c8ccbe8ba6b75a14c188aaabf4b
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-17 16:39:53 +0000

    getter and setter stack methods added

--- IntegerNode.java
+++ IntegerNode.java
@@ -2,11 +2,14 @@ public class IntegerNode {
     private int n;
     private IntegerNode next;
 	private IntegerNode temp;
+	private IntegerNode prev;
+	private int count = 0;
 
     public IntegerNode(int n) {
 	  this.n = n;
 	  this.next = null;
 	  this.temp = null;
+	  this.prev = null;
     }
 
     public int getNumber() {
@@ -17,12 +20,26 @@ public class IntegerNode {
 	  return next;
     }
 
+     public IntegerNode getPrev() {
+		  return prev;
+    }
+
+	public IntegerNode setNextTop(IntegerNode newNode) {
+		  this.next = newNode;
+		  return next;
+	    }
+
+	public IntegerNode getNextTop() {
+			  return next;
+	}
+
     public void setNext(IntegerNode n) {
 	  		if (this.next == null){
 				this.next = n;
 		} else {
 	  			this.next.setNext(n);
 			}
+			count++;
     }
     public void setNextSort(IntegerNode newNode) {
 	  		if (newNode.n >= this.n){
@@ -40,6 +57,30 @@ public class IntegerNode {
 			this.next.next = temp;
 			}
     }
+    public void setNextDL(IntegerNode n) {
+		  		if (this.next == null){
+					this.next = n;
+					this.next.prev = this;
+			} else {
+		  			this.next.setNextDL(n);
+				}
+			count++;
+
+		}
+
+	public boolean deletePrev(IntegerNode n){
+					if (this.next == null){
+						return false;
+					} else if (this.next.n == n.n){
+						this.next = next.next;
+						this.prev = prev.prev;
+						return true;
+					} else {
+						return this.next.deletePrev(n);
+					}
+		}
+
+
     public void printList(){
 			System.out.println(n);
 				if (next != null){
