37111549a9b1cd62a8f35cf70a9c6b8c31096eeb
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-17 13:54:16 +0000

    testing code added and complete

--- Queues.java
+++ Queues.java
@@ -1,7 +1,5 @@
 public class Queues {
 
-	private Queue newQueue = null;
-
 	public static void main(String[] args){
 
 		Queues AQueue = new Queues();
@@ -10,26 +8,28 @@ public class Queues {
 
 		//add ints.
 		public void launch(){
-			IntegerNode l1 = new IntegerNode(45);
-			newQueue.head = l1;
-			IntegerNode l2 = new IntegerNode(22);
+			Queue newQueue = new Queue();
+			System.out.println("There are " + newQueue.size() + " requests in the queue");
+			IntegerNode l1 = new IntegerNode(5);
+			System.out.println("Inserting request " + l1.getNumber());
+			newQueue.insert(l1);
+			IntegerNode l2 = new IntegerNode(8);
+			System.out.println("Inserting request " + l2.getNumber());
 			newQueue.insert(l2);
-			IntegerNode l3 = new IntegerNode(30);
+			IntegerNode l3 = new IntegerNode(12);
+			System.out.println("Inserting request " + l3.getNumber());
 			newQueue.insert(l3);
-			IntegerNode l4 = new IntegerNode(41);
+			System.out.println("There are " + newQueue.size() + " requests in the queue");
+			System.out.println("Retrieving request " + newQueue.retrieve().getNumber() + ".... done.");
+			IntegerNode l4 = new IntegerNode(13);
+			System.out.println("Inserting request " + l4.getNumber());
 			newQueue.insert(l4);
-			IntegerNode l5 = new IntegerNode(18);
-			newQueue.insert(l5);
-			IntegerNode l6 = new IntegerNode(12);
-			newQueue.insert(l6);
-			IntegerNode l7 = new IntegerNode(75);
-			newQueue.insert(l7);
-			IntegerNode l8 = new IntegerNode(36);
-			newQueue.insert(l8);
-			IntegerNode l9 = new IntegerNode(25);
-			newQueue.insert(l9);
-			IntegerNode l10 = new IntegerNode(26);
-			newQueue.insert(l10);
+			System.out.println("There are " + newQueue.size() + " requests in the queue");
+			System.out.println("Retrieving request " + newQueue.retrieve().getNumber() + ".... done.");
+			System.out.println("Retrieving request " + newQueue.retrieve().getNumber() + ".... done.");
+			System.out.println("There are " + newQueue.size() + " requests in the queue");
+			System.out.println("Retrieving request " + newQueue.retrieve().getNumber() + ".... done.");
+			System.out.println("There are " + newQueue.size() + " requests in the queue");
 	}
 
 
