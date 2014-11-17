8f3f11ca924961ddf5896412311e2cc97a745cbc
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-17 13:10:56 +0000

    first commit

---
+++ Queue.java
@@ -0,0 +1,20 @@
+public class Queue {
+
+	private static IntegerNode head = null;
+
+	public Queue(){
+		this.head = head;
+	}
+
+		public void insert(IntegerNode newNode) {
+
+    }
+
+		public void retrieve(){
+
+		}
+
+		public int size(){
+			return 0;
+		}
+}
