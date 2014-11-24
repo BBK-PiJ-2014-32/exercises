8ce397e63b69eebd367a85683191c545a733492f
unknown <phanna02@CSPC055.dcs.bbk.ac.uk>
2014-11-24 20:01:36 +0000

    run method added

--- Supermarket.java
+++ Supermarket.java
@@ -2,18 +2,29 @@ public class Supermarket {
 
 		public static void main(String [] args){
 			Supermarket supMar = new Supermarket();
-			Person per1 = new Person("John");
-			Person per2 = new Person("Simon");
-			supMar.addPerson(per1);
+			supMar.run();
 
 		}
+
+		public void run(){
+			Person per1 = new Person("John");
+			addPerson(per1);
+			Person per2 = new Person("Simon");
+			addPerson(per2);
+			Person per3 = new Person("Mike");
+			addPerson(per3);
+			Person per4 = new Person("Dave");
+		}
+
 
 		public void addPerson(Person person){
 				person.insert(person);
 		}
 
-		public void servePerson(){
-				person.retri
+		public Person servePerson(){
+				Person nextPer = null;
+				//nextPer.retrieve();
+				return nextPer;
 
 		}
 }
