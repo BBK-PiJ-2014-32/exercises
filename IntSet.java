b1c4c9d0dae6e6b7610518d755d1166222168ddc
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-30 21:52:42 +0000

    lower case i added to interface

--- IntSet.java
+++ IntSet.java
@@ -1,15 +1,15 @@
-public Interface IntSet{
+public interface IntSet{
 
 	//adds a new int to the set; if it is there already, nothing happens.
-	void add(int);
+	void add(int newNumber);
 	
 	//returns true if the number is in the set, false otherwise.
-	boolean contains(int); 
+	boolean contains(int newNumber); 
 	
-	/*returns the same values as the former method, but for every element that is checked print
+	/**returns the same values as the former method, but for every element that is checked print
 	*its value on screen.
 	*/
-	containsVerbose(int);
+	boolean containsVerbose(int newNumber);
 
 	//returns a string with the values of the elements in the set separated by commas.
 	String toString();
