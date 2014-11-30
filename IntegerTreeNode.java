fac6b9fd13995e21c6dd4cd2e12923c706713b4b
Phil Hannant <philip.a.hannant@gmail.com>
2014-11-30 16:31:39 +0000

    exercise complete

--- IntegerTreeNode.java
+++ IntegerTreeNode.java
@@ -3,7 +3,11 @@ public class IntegerTreeNode{
 	IntegerTreeNode left;
 	IntegerTreeNode right;
 	
-	public add(int newNumber) {
+	public IntegerTreeNode(int newNumber){
+		value = newNumber;
+	}
+	
+	public void add(int newNumber) {
 		if (newNumber > this.value) {
 			if (right == null) {
 				right = new IntegerTreeNode(newNumber);
@@ -18,5 +22,37 @@ public class IntegerTreeNode{
 			}
 		}
 	}
+	public boolean contains(int n) {
+		if (n == this.value) {
+			return true;
+		} else if (n > this.value) {
+			if (right == null) {
+				return false;
+			} else {
+				return right.contains(n);
+			}
+		} else {
+			if (left == null) {
+				return false;
+			} else {
+				return left.contains(n);
+			}
+		}
+	}
+	
+	public int getMax(){
+		if (right == null){
+			return value;
+		} 
+			return this.right.getMax();
+	}
+	public int getMin(){
+		if (left == null){
+			return value;
+		} 
+			return this.left.getMin();
+	}
+
+
 
 }
