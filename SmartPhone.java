d18e1c7bd834dfdc827ba6ff421360f8f4633139
unknown <phanna02@CSPC063.dcs.bbk.ac.uk>
2014-12-01 19:03:20 +0000

    methods added

--- SmartPhone.java
+++ SmartPhone.java
@@ -1,3 +1,10 @@
 public class SmartPhone extends MobilePhone{
 
+	public void browseWeb(String str){
+		System.out.println(str + " cannot be found.");
+	}
+
+	public void findPosition(){
+		System.out.println("You are on 51st Street, NYC");
+	}
 }
