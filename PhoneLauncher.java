ce4a6425c987111a4f3d0b87b506f8dee42df4ea
unknown <phanna02@CSPC063.dcs.bbk.ac.uk>
2014-12-01 20:07:07 +0000

    contructors tested

--- PhoneLauncher.java
+++ PhoneLauncher.java
@@ -5,12 +5,20 @@ public class PhoneLauncher {
 		launcher.launch();
 	}
 	public void launch() {
-		SmartPhone myPhone = new SmartPhone();
+		SmartPhone myPhone = new SmartPhone(null, null, "Apple");
 		myPhone.browseWeb("www.notanaddress.comish");
 		myPhone.findPosition();
 		myPhone.rignAlarm("5:00am");
 		myPhone.playGame();
 		myPhone.call("999");
 		myPhone.call("0007772");
+		String brandTest1 = myPhone.getBrand();
+		System.out.println(brandTest1);
+		MobilePhone myPhone2 = new MobilePhone(null, "Nokia");
+		String brandTest2 = myPhone2.getBrand();
+		System.out.println(brandTest2);
+		OldPhone myPhone3 = new OldPhone("Binatone");
+		String brandTest3 = myPhone3.getBrand();
+		System.out.println(brandTest3);
 	}
 }
