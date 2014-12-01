4b19a71077db1e750f8569aa69a7f154fc5dc67a
unknown <phanna02@CSPC063.dcs.bbk.ac.uk>
2014-12-01 19:13:21 +0000

    launched and complete

---
+++ PhoneLauncher.java
@@ -0,0 +1,15 @@
+public class PhoneLauncher {
+
+	public static void main(String[] args) {
+		PhoneLauncher launcher = new PhoneLauncher();
+		launcher.launch();
+	}
+	public void launch() {
+		SmartPhone myPhone = new SmartPhone();
+		myPhone.browseWeb("www.notanaddress.comish");
+		myPhone.findPosition();
+		myPhone.rignAlarm("5:00am");
+		myPhone.playGame();
+		myPhone.call("999");
+	}
+}
