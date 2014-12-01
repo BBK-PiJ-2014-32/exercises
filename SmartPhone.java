b6a0fd2c776a0b2d06f14bd47a0a371d21211d2f
unknown <phanna02@CSPC063.dcs.bbk.ac.uk>
2014-12-01 19:41:04 +0000

    call method added with super

--- SmartPhone.java
+++ SmartPhone.java
@@ -7,4 +7,13 @@ public class SmartPhone extends MobilePhone{
 	public void findPosition(){
 		System.out.println("You are on 51st Street, NYC");
 	}
+
+	@Override
+	public void call(String number){
+		if(number.substring(0,2).equals("00")){
+			System.out.println("Calling <" + number + "> through the internet to save money");
+		} else {
+		super.call(number);
+		}
+	}
 }
