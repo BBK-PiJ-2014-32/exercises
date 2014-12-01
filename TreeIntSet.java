fa26e706f37f7046116791b1d666cb93329400d1
Phil Hannant <philip.a.hannant@gmail.com>
2014-12-01 12:47:00 +0000

    first commit

---
+++ TreeIntSet.java
@@ -0,0 +1,15 @@
+public class TreeIntSet implements IntSet{
+
+	
+	public void add(int newNumber){
+	}
+	
+	public boolean contains(int newNumber){
+	}
+	
+	public boolean containsVerbose(int newNumber){
+	}
+
+	public String toString(){
+	}
+}
