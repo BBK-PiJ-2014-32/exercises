6e6ff599406fb29e372cc841e9a7672b917227a1
Phil Hannant <philip.a.hannant@gmail.com>
2014-12-04 14:25:21 +0000

    first commit

---
+++ Guitar.java
@@ -0,0 +1,9 @@
+public class Guitar implements WoodenObject, MusicalInstrument{
+
+}
+
+	public static void main(Strin[] args){
+		Guitar myGuit = new Guitar();
+		myGuit.play();
+		myGuit.burn();
+	}
