94461e78acc7ebddd205ffd4abdbc274a95da8c8
Phil Hannant <philip.a.hannant@gmail.com>
2014-12-05 14:37:03 +0000

    first commit

---
+++ NoahsArkScript.java
@@ -0,0 +1,11 @@
+public class NoahsArkScript{
+
+	public static void main(String[] args){
+		NoahsArkSctipt newArk = new NoahsArkSctip();
+		newArk.run();
+	}
+
+	public void run();
+	Animal bear = new Mammal("Polar Bear", "Growl");
+
+}
