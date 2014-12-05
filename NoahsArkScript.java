d7e35677e16a82be569a263209e3586a25cf74d6
Phil Hannant <philip.a.hannant@gmail.com>
2014-12-05 15:08:31 +0000

    Script added

--- NoahsArkScript.java
+++ NoahsArkScript.java
@@ -1,11 +1,40 @@
 public class NoahsArkScript{
 
 	public static void main(String[] args){
-		NoahsArkSctipt newArk = new NoahsArkSctip();
+		NoahsArkScript newArk = new NoahsArkScript();
 		newArk.run();
 	}
 
-	public void run();
-	Animal bear = new Mammal("Polar Bear", "Growl");
-
+	public void run(){
+	Animal bears = new Mammals("Bears", "Growl");
+	call.bears();
+	Animal beatles = new Insects("Beatles", "Grunt");
+	call.beatles();
+	Animal cats = new Mammals("Lions", "Roar");
+	call.cats();
+	Animal crocodiles = new Reptiles("Crocs", "Sneer");
+	call.crocodiles();
+	Animal dolphins = new Mammals("Dolphins", "Click");
+	call.dolphins();
+	Animal eagles = new Birds("Eagles", "Call");
+	call.eagles();
+	Animal flies = new Insects("Flies", "Buzz");
+	call.flies();
+	Animal frogs = new Amphibians("Frogs", "Ribbet");
+	call.frogs();
+	Animal lizards = new Reptiles("Lizards", "Chirp");
+	call.lizards();
+	Animal monkeys = new Mammals("Monkeys", "Chatter");
+	call.monkeys();
+	Animal pigeons = new Birds("Pigeons", "Coo");
+	call.pigeons();
+	Animal salmon = new Fish("Salmon", "Burp");
+	call.salmon();
+	Animal sharks = new Fish("Sharks", "Grunt");
+	call.sharks();
+	Animal snakes = new Reptiles("Snakes", "Sssss");
+	call.snakes();
+	Animal whales = new Mammals("Whales", "Click");
+	call.whales();
+	}
 }
