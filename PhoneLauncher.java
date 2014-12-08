f756245b1bb73a608f2bed596b05814200150e6b
Phil Hannant <philip.a.hannant@gmail.com>
2014-12-08 20:13:30 +0000

    only methods from the interface

--- PhoneLauncher.java
+++ PhoneLauncher.java
@@ -6,11 +6,19 @@ public class PhoneLauncher {
 	}
 	public void launch() {
 		MobilePhone myPhone = new SmartPhone();
+		testPhone(myPhone);
 		myPhone.browseWeb("www.notanaddress.comish");
 		myPhone.findPosition();
 		myPhone.rignAlarm("5:00am");
 		myPhone.playGame();
 		myPhone.call("999");
 		myPhone.call("0007772");
+
 		}
+
+	public void testPhone(Phone phone){
+		phone.call("999");
+
+	}
+
 }
