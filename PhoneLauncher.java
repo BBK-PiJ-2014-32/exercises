752b1e502d2bc9f9644961b50fd8a58c10a9ccf2
Phil Hannant <philip.a.hannant@gmail.com>
2014-12-08 18:31:07 +0000

    script added

--- PhoneLauncher.java
+++ PhoneLauncher.java
@@ -6,6 +6,11 @@ public class PhoneLauncher {
 	}
 	public void launch() {
 		SmartPhone myPhone = new SmartPhone();
-
-	}
+		myPhone.browseWeb("www.notanaddress.comish");
+		myPhone.findPosition();
+		myPhone.rignAlarm("5:00am");
+		myPhone.playGame();
+		myPhone.call("999");
+		myPhone.call("0007772");
+		}
 }
