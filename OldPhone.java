734c8e9495232ebe6979cc38f199bd2227f9efbd
Phil Hannant <philip.a.hannant@gmail.com>
2014-12-08 18:25:44 +0000

    removed getBrand method

--- OldPhone.java
+++ OldPhone.java
@@ -1,8 +1,5 @@
 public class OldPhone implements Phone{
 
-		public String getBrand(){
-			return brand;
-		}
 
 		public void call(String number){
 			System.out.println("Calling <" + number + ">");
