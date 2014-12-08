f7a62432eea1a7f86dc16ddd6db4d60912239233
Phil Hannant <philip.a.hannant@gmail.com>
2014-12-08 18:17:34 +0000

    initial script created

--- PhoneLauncher.java
+++ PhoneLauncher.java
@@ -5,20 +5,7 @@ public class PhoneLauncher {
 		launcher.launch();
 	}
 	public void launch() {
-		SmartPhone myPhone = new SmartPhone("Apple");
-		myPhone.browseWeb("www.notanaddress.comish");
-		myPhone.findPosition();
-		myPhone.rignAlarm("5:00am");
-		myPhone.playGame();
-		myPhone.call("999");
-		myPhone.call("0007772");
-		String brandTest1 = myPhone.getBrand();
-		System.out.println(brandTest1);
-		MobilePhone myPhone2 = new MobilePhone("Nokia");
-		String brandTest2 = myPhone2.getBrand();
-		System.out.println(brandTest2);
-		OldPhone myPhone3 = new OldPhone("Binatone");
-		String brandTest3 = myPhone3.getBrand();
-		System.out.println(brandTest3);
+		SmartPhone myPhone = new SmartPhone();
+
 	}
 }
