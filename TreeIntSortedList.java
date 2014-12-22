8c94fe2ad459c110ba651ff5b70d47e0be573c98
Phil Hannant <philip.a.hannant@gmail.com>
2014-12-22 22:48:47 +0000

    toString() now printing all values

--- TreeIntSortedList.java
+++ TreeIntSortedList.java
@@ -1,24 +1,24 @@
 public class TreeIntSortedList implements IntSortedList{
 	int value;
-	TreeIntSet left;
-	TreeIntSet right;
+	TreeIntSortedList left;
+	TreeIntSortedList right;
 
-	public TreeIntSet(int newNumber){
+	public TreeIntSortedList(int newNumber){
 		value = newNumber;
 	}
 
 	public void add(int newNumber){
-		if (newNumber > this.value) {
-			if (right == null) {
-					right = new TreeIntSet(newNumber);
+		if (newNumber >= this.value) {
+			if (this.right == null) {
+					this.right = new TreeIntSortedList(newNumber);
 			} else {
-					right.add(newNumber);
+					this.right.add(newNumber);
 			}
 		} else {
-			if (left == null) {
-					left = new TreeIntSet(newNumber);
+			if (this.left == null) {
+					this.left = new TreeIntSortedList(newNumber);
 			} else {
-					left.add(newNumber);
+					this.left.add(newNumber);
 				}
 			}
 
@@ -41,26 +41,6 @@ public class TreeIntSortedList implements IntSortedList{
 		}
 	}
 
-
-	public boolean containsVerbose(int n){
-		System.out.println(this.value);
-		if (n == this.value) {
-				return true;
-		} else if (n > this.value) {
-				if (right == null) {
-					return false;
-				} else {
-					return right.containsVerbose(n);
-				}
-		} else {
-				if (left == null) {
-					return false;
-				} else {
-					return left.containsVerbose(n);
-				}
-		}
-	}
-
 	public String toString(){
 		    String str = "";
 		       if(right != null && left != null) {
@@ -72,6 +52,8 @@ public class TreeIntSortedList implements IntSortedList{
 			   } else if (left != null && right == null){
 				   str += this.value + ", ";
 			       str += left.toString();
+			   } else if (left == null && right == null){
+				   str += this.value + ", ";
 			   }
 		       return str;
 	}
