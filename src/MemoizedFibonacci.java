9f09439501fb7bd13cbea5aadb92de138159133f
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-19 13:54:05 +0000

    javadecaf code shell added

--- src/MemoizedFibonacci.java
+++ src/MemoizedFibonacci.java
@@ -1,4 +1,22 @@
 
 public class MemoizedFibonacci {
 
+	public static void main(String[] args){
+	System.out.println("Please enter a number: ");
+	String str = System.console().readLine();
+	int x = Integer.parseInt(str);
+	
+	fib(x);
+	int result = fib(x);
+	System.out.println("The " + x + "th term of the Fibonacci sequence is " + result);
+	}
+	
+	public static int fib(int n) {
+		if ((n == 1) || (n == 2)) {
+			return 1;
+		} else {
+			int result = fib(n-1) + fib(n-2);
+			return result;
+			}
+		}
 }
