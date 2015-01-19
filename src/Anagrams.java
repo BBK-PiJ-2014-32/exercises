617c2b2699af470cd8899caff5b0271365fc8fd5
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-19 19:18:12 +0000

    dodgy code

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -4,23 +4,29 @@ import java.util.List;
 
 public class Anagrams {
 
-	public static ArrayList<String> anagramCreater(String input){
-		ArrayList<String> anagramList = new ArrayList<String>();
-		return anagramList;
+	private static ArrayList<String> anagramList = null;
+	
+	public static List<String> anagramCreator(String input){
+	List<String> anaList = new ArrayList<>();
+	char first = input.charAt(0);
+	String rest = input.substring(1);
+	
+		List<String> anagrams = anagramCreator(rest);
+			for (String str : anagrams){
+				for (int i = 0; i <= str.length(); i++){
+					anaList.add(insertChar(str, first, i));
+				}
+			}
+		return anaList;	
 	}
 	
-	public void anag(String s1, String s2)  {
-        if(s1.length() == 0) {
-            System.out.println(s2);
-        }
-	        for(int i = 0; i < s1.length(); i++) {
-	            anag(s1.substring(0, i) + s1.substring(i+1, s1.length()), s1.charAt(i) + s2);
-	        }
-	    }
+	public static String insertChar(String str, char c, int j){
+		String start = str.substring(0, j);
+		String end = str.substring(j);
+		return start + c + end;
+	}
 
-	public static void main(String[] args)  {
-        Anagrams ana = new Anagrams();
-        ana.anag("every", "");
-	    }
-
+	public static void main(String[] args){
+		System.out.println(anagramCreator("tes"));
+	}
 }
