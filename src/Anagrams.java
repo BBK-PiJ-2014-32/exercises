9d4330b1dc0bd6e65581cef09fa864a474d4a684
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-20 21:21:13 +0000

    Moving onto something else

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -5,29 +5,33 @@ public class Anagrams {
 
 	private static ArrayList<String> anagramList = new ArrayList<>();
 	
-	public static List<String> anagramCreator (String input){
-	anagramList.add(create(input, ""));
-	return anagramList;
+	public static List<String> anagramCreator (String input, String first){
+		ArrayList<String> anagList = new ArrayList<>();
+		if(input.length() == 0) {
+			anagList.add(first);
+			System.out.println(anagList.size());
+		}	for(int i = 0; i < input.length(); i++) {
+			System.out.println(anagList.size());
+				create(input.substring(0, i) + input.substring(i+1, input.length()), input.charAt(i) + first);
+			}return anagList;
 	}
 
 	public static String create(String input, String first){
+		
 		if(input.length() == 0) {
 			return first;
 		}	for(int i = 0; i < input.length(); i++) {
-				String str = create(input.substring(0, i) + input.substring(i+1, input.length()), input.charAt(i) + first);
-				anagramList.add(str);
-				return str;
+				System.out.println(create(input.substring(0, i) + input.substring(i+1, input.length()), input.charAt(i) + first));
+				return null;
 			}
 		return null;
 		}
 		
 	public static void main(String[] args){
 		Anagrams ana = new Anagrams();
-		ana.anagramCreator("tea");
-		System.out.println(anagramList.size());
-		for (int i = 0; i < anagramList.size(); i++){
-			System.out.println(anagramList.get(i));
-		}
+		List <String> anaList  = new ArrayList<>();
+		System.out.println(ana.anagramCreator("tea", "").size());
+		
 	}
 
 }
