e397a61cfe1e8c7394ecd56868a379bfb9d57c3b
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-22 11:27:42 +0000

    exercise complete

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -3,19 +3,25 @@ import java.util.ArrayList;import java.util.List;
 
 public class Anagrams {
 
-	public static String create(String prefix, String input){
+	private static ArrayList<String> anaList = new ArrayList<>();
+	
+	public Anagrams(){
+		this.anaList = anaList;
+	}
+		
+	public static ArrayList<String> create(String prefix, String input){
 		if(input.length() == 0){
-			return prefix;
+			anaList.add(prefix);
 		} else {
 			for(int i = 0; i < input.length(); i++){
 					create((prefix + input.charAt(i)), (input.substring(0, i) + input.substring(i+1)));
-				}	
-		} return prefix;
+			}	
+		} return anaList;
 	}
 	
 	public static void main(String[] args){
 		Anagrams ana = new Anagrams();
-		System.out.println(ana.create("", "abc"));
+		System.out.println(ana.create("", "every"));
 		
 		
 	}
