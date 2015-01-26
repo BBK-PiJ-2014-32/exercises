53bde9ca251fae3c4ae2ac0d6dd12f432606c04b
Phil Hannant <philip.a.hannant@gmail.com>
2015-01-26 20:20:43 +0000

    first commit

---
+++ src/day15/PrimeDivisorList.java
@@ -0,0 +1,21 @@
+package day15;
+
+public class PrimeDivisorList implements PrimeDivisor {
+
+	@Override
+	public void add(Integer input) {
+		
+		
+	}
+
+	@Override
+	public void remove(int index) {
+		
+		
+	}
+
+	@Override
+	public String toString(){
+		return null;
+	}
+}
