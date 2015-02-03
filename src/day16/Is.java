0b4c0ad9387f50990be61b8e1a487f70ff2840ec
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-03 18:23:53 +0000

    exercise complete

--- src/day16/Is.java
+++ src/day16/Is.java
@@ -1,5 +1,24 @@
 package day16;
+import java.*;
+import java.io.File;
 
 public class Is {
+	
+	public static void main(String [] args){
+	
+		File f = null;
+		String [] paths;
+	try{	
+		f = new File ("/Users/philhannant/git/Day16/");
+		
+		paths = f.list();
+		
+		for(String path:paths){
+			System.out.println(path);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+	}
+	}
 
 }
