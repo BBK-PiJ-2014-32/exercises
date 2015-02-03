1fd0585a2fc576aefb2b334f431e261576b66d0a
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-03 21:46:33 +0000

    Code shell added

--- src/day16/Cat.java
+++ src/day16/Cat.java
@@ -1,16 +1,29 @@
 package day16;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Cat {
 
 	public static void main(String[] args){
-		
+		Cat theCat = new Cat();
+		theCat.run();
 	}
 	
 	public void run(){
+		File f = null;
 		
-	}
+		try{
+			f = new File (getInput()); 
+				if(f.exists()){
+					System.out.println(f);
+				}
+			
+			} catch(NullPointerException e) {
+				e.printStackTrace();
+			}
+		}
+
 	
 	public String getInput(){
 		System.out.println("Please enter directory name: ");
