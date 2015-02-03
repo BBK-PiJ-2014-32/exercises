b6f2a87131bc88070cda358d9a87346cf937c4b3
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-03 22:09:15 +0000

    excpetions and code body added

--- src/day16/Cat.java
+++ src/day16/Cat.java
@@ -1,6 +1,10 @@
 package day16;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Cat {
@@ -12,15 +16,34 @@ public class Cat {
 	
 	public void run(){
 		File f = null;
-		
+		BufferedReader br = null;
 		try{
 			f = new File (getInput()); 
-				if(f.exists()){
-					System.out.println(f);
+				if(f.exists() == true){
+					br = new BufferedReader(new FileReader(f));
+					 String line = null;
+					 while ((line = br.readLine()) != null) {
+					   System.out.println(line);
+					   br.close();
+					 }
+				} else {
+					System.out.println("File does not exist");
 				}
 			
-			} catch(NullPointerException e) {
-				e.printStackTrace();
+			} catch(NullPointerException ex) {
+				ex.printStackTrace();
+			} catch (FileNotFoundException ex) {
+				System.out.println("File " + f + " does not exist.");
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}	finally {
+						try {
+							if (br != null) {
+								br.close();
+							}
+						} catch (IOException ex) {
+							ex.printStackTrace();
+						}
 			}
 		}
 
