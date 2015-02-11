8dc68f64f70aa589f2188f13a79e5947c56e0569
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-11 13:41:10 +0000

    getInput adapted

--- src/day17/ResponsiveUI.java
+++ src/day17/ResponsiveUI.java
@@ -16,7 +16,7 @@ public class ResponsiveUI implements Runnable {
 	
 	public static void main(String[] args){
 		for(int i = 0; i <10; i++){
-			ResponsiveUI UI = new ResponsiveUI(getInput());
+			ResponsiveUI UI = new ResponsiveUI(getInput(i));
 			Thread t = new Thread(UI);
 			t.start();
 				synchronized(t){
@@ -31,13 +31,13 @@ public class ResponsiveUI implements Runnable {
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
-	public static int getInput(){
+	public static int getInput(int i){
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Please enter a time in milliseconds: ");
+		System.out.println("Please enter a time in milliseconds for task " + i + ": ");
 		String str = scan.nextLine();
 		int input = Integer.parseInt(str);
 		scan.close();
