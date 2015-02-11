a1490b12c983e42cf0fc0546e9245d9691221c66
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-11 13:26:52 +0000

    getInput method added

--- src/day17/ResponsiveUI.java
+++ src/day17/ResponsiveUI.java
@@ -1,11 +1,37 @@
 package day17;
 
+import java.util.Scanner;
+
+
+
 public class ResponsiveUI implements Runnable {
 
+	private ResponsiveUI r;
+	private Scanner scan;
+	
+	public ResponsiveUI(int input){
+		
+	}
+	
+	public static void main(String[] args){
+		ResponsiveUI UI = new ResponsiveUI(getInput());
+		for(int i = 0; i <10; i++){
+			
+		}
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	public static int getInput(){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a time in milliseconds: ");
+		String str = scan.nextLine();
+		int input = Integer.parseInt(str);
+		scan.close();
+		return input;
+	}
 }
