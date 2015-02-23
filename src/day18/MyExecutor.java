35402bc467d6b0b1826a7b58615f29373798f13c
Phil Hannant <philip.a.hannant@gmail.com>
2015-02-23 19:25:53 +0000

    first commit

---
+++ src/day18/MyExecutor.java
@@ -0,0 +1,13 @@
+package day18;
+
+import java.util.concurrent.*;
+
+public class MyExecutor implements Executor{
+
+	@Override
+	public void execute(Runnable command) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
